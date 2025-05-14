607e1a1a3b85117daaa94ecc9e6d687bb83a48b3
Martin Riedl <martin.riedl@mailbox.org>
2025-05-14 15:42:59 +0200

    Working on PDP and PEP interplay at beginning. check_mandatory_params() looking good for now.

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    test = {'name': 'martin'}
+    single = {'name'}
+    print(type(single), type(test.keys()))
+    assert test.keys() == single
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
